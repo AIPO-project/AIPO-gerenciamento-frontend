246e38e066437a099231cbba20d23ccde5b16e75
Vitória Freire <vitoriafs200703@gmail.com>
2025-05-27 16:20:12 -0300

    inicio do desenvolvimento de página de login

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const matricula = document.getElementById('matricula').value;
+    const senha = document.getElementById('senha').value;
+
+    try {
+        const response = await fetch('http://127.0.0.1:5000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ matricula, senha })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+        console.log(data.message);
+        } else {
+        console.log(data.message)
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        console.log('Erro ao enviar os dados');
+    }
+});
